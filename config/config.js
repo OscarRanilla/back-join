bc000792e17f3012084c2972a610df7ed4dfb6de
OscarRanilla <oscarjosemanuel89@gmail.com>
2025-04-03 20:43:18 +0200

    Cambios realizados al ... 03/04/2025

--- config/config.js
+++ config/config.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 const dbConnection = async() => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log('Base de datos conectada con éxito')
+    console.log(' ✅ Base de datos conectada con éxito ')
   } catch (error) {
-    console.log(error)
+    console.error(" ❌ Error conectando a MongoDB:", error)
   }
 } 
 
